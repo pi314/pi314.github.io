5fc68c54e40f43541168267fe0dcd62201e9b97b
Cychih <michael66230@gmail.com>
2015-03-07 13:12:03 +0800

    parse article content, preserve white spaces

--- js/article-content-manager.js
+++ js/article-content-manager.js
@@ -18,7 +18,14 @@ ArticleContentManager.handle_article_content = function (index, raw_content) {
     lines = raw_content.split(/\r?\n/g);
 
     var in_header = true;
-    var article_info = {};
+    var article_info = {
+        'like':'true',
+        'datetime':'yyyy/mm/dd.HH:MM',
+        'week':'What',
+        'sign':'nctu',
+        'title':'(empty)',
+        'push':'',
+    };
     for (var l = 0; l < lines.length; l++) {
         console.log(lines[l], in_header);
         // still in article header
@@ -29,22 +36,24 @@ ArticleContentManager.handle_article_content = function (index, raw_content) {
 
             var match = /^:(.*?): *(.*)$/.exec(lines[l]);
             if (match == null) {
+                ArticleContentManager.commit_header(article_content_object, article_info);
+                article_content_object.append('<div class="line"><div class="text-block">'+ lines[l] +'</div></div>');
                 in_header = false;
+                continue;
             }
+            article_info[ match[1] ] = match[2];
             console.log(match);
 
             if (in_header == false) {
-                // commit article header into container
-                continue;
+                ArticleContentManager.commit_header(article_content_object, article_info);
             }
-            article_content_object.append('<div class="line"><div class="text-block">['+ match[1] +']:['+ match[2] +']</div></div>');
             continue;
         }
 
         // article content, plane text
         if (lines[l] == '') {
             // this does not work, I need another method to display an empty line
-            article_content_object.append('<div class="line"><div class="text-block"> </div></div>');
+            article_content_object.append('<div class="line"><div class="text-block"><br></div></div>');
         } else {
             article_content_object.append('<div class="line"><div class="text-block">'+ lines[l] +'</div></div>');
         }
@@ -57,4 +66,36 @@ ArticleContentManager.handle_article_content = function (index, raw_content) {
     // this article is ready now
     articles_ready[index] = true;
 
+};
+
+ArticleContentManager.commit_header = function (article_content_object, article_info) {
+    var header_line;
+    var header_prompt;
+    var header_value;
+    header_line = $('<div class="line article-header"></div>');
+    header_prompt = $('<div class="text-block prompt">作者</div>');
+    header_value = $('<div class="text-block value">pi314 (圓周率)</div>');
+    header_line.append(header_prompt);
+    header_line.append(header_value);
+    header_prompt = $('<div class="text-block prompt right">站內</div>');
+    header_value = $('<div class="text-block value right">P_pi314</div>');
+    header_line.append(header_value);
+    header_line.append(header_prompt);
+    article_content_object.append(header_line);
+
+    header_line = $('<div class="line article-header"></div>');
+    header_prompt = $('<div class="text-block prompt">標題</div>');
+    header_value = $('<div class="text-block value">'+ article_info['title'] +'</div>');
+    header_line.append(header_prompt);
+    header_line.append(header_value);
+    article_content_object.append(header_line);
+
+    header_line = $('<div class="line article-header"></div>');
+    header_prompt = $('<div class="text-block prompt">時間</div>');
+    var d = article_info['datetime'].split('.');
+    header_value = $('<div class="text-block value">'+ d[0] +' '+ article_info['week'] +' '+ d[1] +'</div>');
+    header_line.append(header_prompt);
+    header_line.append(header_value);
+    article_content_object.append(header_line);
+    article_content_object.append('<div class="line"><div class="text-block"><br></div></div>');
 };
