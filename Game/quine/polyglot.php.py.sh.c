129f827aeeef1fff81fb141c8369668f2fe31519
Cychih <michael66230@gmail.com>
2015-04-09 01:59:37 +0800

    add Game/quine/polyglot.php.py.sh.c (new: php)

---
+++ Game/quine/polyglot.php.py.sh.c
@@ -0,0 +1,12 @@
+#/*<?php echo "Hello, PHP!*".'/'."#\n";exit(); ?>*/
+#include <stdio.h>/*
+'echo' 'Hello, sh!'
+exit
+print('Hello, Python!')
+"""
+#*/
+int main () {
+    printf("Hello, C!\n");
+}
+#define c//\
+"""
