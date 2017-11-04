117bbf6c722505e147f0a7d5d2b4a32596c3df15
Cychih <michael66230@gmail.com>
2017-11-04 16:56:36 +0800

    Create polyglot.py2.py3.sh.c

---
+++ Game/quine/polyglot.py2.py3.sh.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // \
+'echo' "Hello shell" ; exit ; import sys
+#define p(hello, lang) printf(hello " " lang "\n"); // \
+dummy, LANG, rtn = ('''
+int main () //''', 'Python{}'.format(sys.version_info.major), lambda n:n)
+#define LANG "C" // \
+def p(a, b): print(' '.join((a, b)))
+{
+p("Hello", LANG)
+#define rtn(n) return n; //\
+,
+rtn(0)
+}
