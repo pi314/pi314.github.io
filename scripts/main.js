fe9d1e2d16cc301042b2d2229a356d4760fe7482
Cychih <michael66230@gmail.com>
2019-04-06 00:46:32 +0800

    Use vue to render file list

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,41 @@
+var state = 'list';
+
+
+function article (fname) {
+    this.fname = fname;
+    this.index = 0;
+}
+
+
 $(function () {
+    var articles = [];
 
-    // display state, (*)ST_LIST / ST_ARTICLE
-    StateMachine.state_init();
+    for (var i = 0; i < ARTICLE_FILES_LIST.length; i++) {
+        var atc = new article(ARTICLE_FILES_LIST[i]);
+        atc.index = i + 1;
+        articles.push(atc);
+    }
 
-    $.ajax({
-        url: 'Articles/signatures.txt',
-        cache: false,
-    }).done(function (msg) {
-        ArticleContentManager.handle_signatures(msg);
+    var app = new Vue({
+        el: '#articles-list-panel',
+        data: {
+            message: 'Hello Vue!',
+            articles: articles,
+        }
+    })
 
-        // article contents must be fetched after the signature file
-        ArticleListManager.article_list_panel_init();
-        ArticleListManager.article_list_panel_mouse_binding();
-    });
+    // // display state, (*)ST_LIST / ST_ARTICLE
+    // StateMachine.state_init();
+    //
+    // $.ajax({
+    //     url: 'Articles/signatures.txt',
+    //     cache: false,
+    // }).done(function (msg) {
+    //     ArticleContentManager.handle_signatures(msg);
+    //
+    //     // article contents must be fetched after the signature file
+    //     ArticleListManager.article_list_panel_init();
+    //     ArticleListManager.article_list_panel_mouse_binding();
+    // });
 
 });
